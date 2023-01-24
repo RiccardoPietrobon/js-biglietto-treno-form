56b1db809a4275c86319f21fc7b5c588638a6f91
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-01-24 19:18:45 +0100

    input

--- file_JS/main.js
+++ file_JS/main.js
@@ -1,16 +1,65 @@
+//Nome passegero
+const inputuserName = document.getElementById("userNameIns").value;
+console.log("nome ", inputuserName);
+
+inputuserName.a
+function registrazione() {
+}
+
+
+
 //Il numero di chilometri da percorrere
-const userKM = parseInt(prompt("Quanti km devi percorrere?"));
+const userKM = document.getElementById("userKmIns").value;
+
+console.log("distanza ", userKM);
+
+document.getElementById("stamp_km").innerHTML = userKM;
+
+
 
 //Età del passeggero
-const
+const userAge = document.getElementById("userAgeIns").value;
+console.log("età ", userAge);
+document.getElementById("stamp_age").innerHTML = userAge;
+
+
+
 //Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
 //Il prezzo del biglietto è definito in base ai km(0.21 € al km)
+const priceKM = .21;
+let priceTicket = userKM * priceKM;
+console.log("costo biglietto ", priceTicket);
+
 //Va applicato uno sconto del 20 % per i minorenni
+if (userAge < 18) {
+    //sconto percentuale u18
+    const discountU18 = 0.2;
+    //sconto effettivo
+    const priceDiscountU18 = priceTicket * discountU18;
+    //prezzo biglietto scontato
+    const ticketUnder = priceTicket - priceDiscountU18;
+    console.log("prezzo scontato under è ", ticketUnder);
+
+    finalTicket = "Il prezzo del biglietto sarà di € ", ticketUnder.toFixed(2);
+}
 //Va applicato uno sconto del 40 % per gli over 65.
+else if (userAge > 65) {
+    //sconto percentuale o65
+    const discountO65 = 0.4;
+    //sconto effettivo
+    const priceDiscountO65 = priceTicket * discountO65;
+    //prezzo biglietto scontato
+    const ticketOver = priceTicket - priceDiscountO65;
+    console.log("prezzo scontato over è ", ticketOver);
 
-//MILESTONE 1:
-//Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone(non stilizzati), realizziamo le specifiche scritte sopra.La risposta finale(o output) sarà anch'essa da scrivere in console.
+    finalTicket = "Il prezzo del biglietto sarà di € ", ticketOver.toFixed(2);
 
-//MILESTONE 2:
-//Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l'utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
-//Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
+}
+//Altrimenti nessun sconto
+else {
+    finalTicket = "Il prezzo del biglietto sarà di € ", priceTicket.toFixed(2);
+}
+
+//document.getElementById('biglietto_finale').innerHTML = finalTicket;
+
+
