ab85d71b90208256a056429d9872ec95b39d17ec
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-01-25 18:36:18 +0100

    sistemato condizioni

--- file_JS/main.js
+++ file_JS/main.js
@@ -1,65 +1,86 @@
-//Nome passegero
-const inputuserName = document.getElementById("userNameIns").value;
+//ELEMENTI DI INPUT
+//Button
+const mybutton = document.getElementById("push");
+console.log("buttoooon ", mybutton);
+//Name
+const inputuserName = document.getElementById("userNameIns");
 console.log("nome ", inputuserName);
-
-inputuserName.a
-function registrazione() {
-}
-
-
-
 //Il numero di chilometri da percorrere
-const userKM = document.getElementById("userKmIns").value;
-
-console.log("distanza ", userKM);
-
-document.getElementById("stamp_km").innerHTML = userKM;
-
-
-
+const inputKM = document.getElementById("userKmIns");
+console.log("distanza ", inputKM);
 //Età del passeggero
-const userAge = document.getElementById("userAgeIns").value;
-console.log("età ", userAge);
-document.getElementById("stamp_age").innerHTML = userAge;
+const inputAge = document.getElementById("userAgeIns");
+console.log("età ", inputAge);
+
+
+mybutton.addEventListener("click", function () {
+    //Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+    //Il prezzo del biglietto è definito in base ai km(0.21 € al km)
+    const outputName = inputuserName.value;
+    console.log("nome output ", outputName);
+
+    //età utente
+    const userAge = inputAge.value;
+    console.log("age output ", userAge);
+
+    //km utente
+    const userKM = inputKM.value;
+    console.log("km output ", userKM);
 
 
 
-//Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
-//Il prezzo del biglietto è definito in base ai km(0.21 € al km)
-const priceKM = .21;
-let priceTicket = userKM * priceKM;
-console.log("costo biglietto ", priceTicket);
+    const priceKM = .21;
+    let priceTicket = userKM * priceKM;
+    console.log("costo biglietto ", priceTicket);
+    let finalTicket;
 
-//Va applicato uno sconto del 20 % per i minorenni
-if (userAge < 18) {
-    //sconto percentuale u18
-    const discountU18 = 0.2;
-    //sconto effettivo
-    const priceDiscountU18 = priceTicket * discountU18;
-    //prezzo biglietto scontato
-    const ticketUnder = priceTicket - priceDiscountU18;
-    console.log("prezzo scontato under è ", ticketUnder);
 
-    finalTicket = "Il prezzo del biglietto sarà di € ", ticketUnder.toFixed(2);
+    //Va applicato uno sconto del 20 % per i minorenni
+    if (userAge < 18) {
+        //sconto percentuale u18
+        const discountU18 = 0.2;
+        //sconto effettivo
+        const priceDiscountU18 = priceTicket * discountU18;
+        //prezzo biglietto scontato
+        priceTicket = priceTicket - priceDiscountU18;
+        console.log("prezzo scontato under è ", priceTicket);
+
+        finalTicket = "Il prezzo del biglietto sarà di € ", priceTicket.toFixed(2);
+    }
+
+    //Va applicato uno sconto del 40 % per gli over 65.
+    else if (userAge > 65) {
+        //sconto percentuale o65
+        const discountO65 = 0.4;
+        //sconto effettivo
+        const priceDiscountO65 = priceTicket * discountO65;
+        //prezzo biglietto scontato
+        priceTicket = priceTicket - priceDiscountO65;
+        console.log("prezzo scontato over è ", priceTicket);
+
+        finalTicket = "Il prezzo del biglietto sarà di € ", priceTicket.toFixed(2);
+
+    }
+
+    //Altrimenti nessun sconto
+    else (18 > userAge > 65); {
+
+        finalTicket = "Il prezzo del biglietto sarà di € ", priceTicket.toFixed(2);
+
+    }
+
+
+
+    finalTicketoutput.innerHTML = finalTicket;
+
 }
-//Va applicato uno sconto del 40 % per gli over 65.
-else if (userAge > 65) {
-    //sconto percentuale o65
-    const discountO65 = 0.4;
-    //sconto effettivo
-    const priceDiscountO65 = priceTicket * discountO65;
-    //prezzo biglietto scontato
-    const ticketOver = priceTicket - priceDiscountO65;
-    console.log("prezzo scontato over è ", ticketOver);
-
-    finalTicket = "Il prezzo del biglietto sarà di € ", ticketOver.toFixed(2);
-
-}
-//Altrimenti nessun sconto
-else {
-    finalTicket = "Il prezzo del biglietto sarà di € ", priceTicket.toFixed(2);
-}
-
-//document.getElementById('biglietto_finale').innerHTML = finalTicket;
+)
 
 
+//ELEMENTI DI OUTPUT
+//name
+outputName = document.getElementById("stamp_name");
+//Ticket code
+const outputCode = document.getElementById("stamp_code");
+//Ticket price
+const finalTicketoutput = document.getElementById("stamp_price");
